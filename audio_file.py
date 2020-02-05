88823f07ccb95d69ca177493c0790b2248e77af4
felix ruttan <felix.ruttan@gmail.com>
2020-02-05 15:35:31 -0800

    master

--- audio_file.py
+++ audio_file.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from os import path
 from typing import Dict
 from usage_stats import UsageStats
+from abc import abstractmethod
 
 
 class Audio:
@@ -16,15 +17,12 @@ class Audio:
             raise ValueError("title of the song must be a string")
         if not isinstance(artist, str):
             raise ValueError("the artist must be a string")
-        if not isinstance(album, str):
-            raise ValueError("the name of the album must be a string")
         if not isinstance(runtime, str):
             raise ValueError("the runtime must be in the format hh:mm:ss")
         if not path.exists(pathname + filename):
             raise ValueError("path doesn't exist")
         self._title = title
         self._artist = artist
-        self._album = album
         self._runtime = runtime
         self._rating = None
         self._pathname = pathname
@@ -45,15 +43,10 @@ class Audio:
         """plays the song"""
         self._usage.increment_usage_stats()
 
+    @abstractmethod
     def get_description(self) -> str:
         """returns a description of the song"""
-        if self._rating is None:
-            song_description = "{} by {} from the album {} added on {}. Runtime is {}"\
-            .format(self._title, self._artist,self._album,self._usage.date_added,self._runtime)
-        else:
-            song_description = "{} by {} from the album {} added on {}. Runtime is {}. User Rating is {}"\
-                .format(self._title, self._artist, self._album,self._usage.date_added, self._runtime, self._rating)
-        return song_description
+        pass
 
     def get_location(self) -> str:
         return self._pathname + self._filename
@@ -61,19 +54,9 @@ class Audio:
     def get_usage_stats(self) -> UsageStats:
         return self._usage
 
+    @abstractmethod
     def meta_data(self) -> Dict:
-        meta_data_dict = {
-            'title': self._title, 
-            'artist': self._artist, 
-            'album': self._album,
-            'date_added': self._usage.date_added, 
-            'runtime': self._runtime, 
-            'pathname': self._pathname,
-            'filename': self._filename, 
-            'play_count': self._usage.play_count,
-            'last_played': self._usage.last_played, 
-            'rating': self._rating}
-        return meta_data_dict
+       pass
 
     def get_play_count(self):
         return self._usage.play_count
