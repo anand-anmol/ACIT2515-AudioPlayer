909f53387eb4a38eb9154ec3d3361c75d2c2e73d
anmols2007 <anmols2007@gmail.com>
2020-02-05 17:08:21 -0800

    fixed minor bugs in the playlist class

--- playlist.py
+++ playlist.py
@@ -12,15 +12,15 @@ class PlayList:
 
     def __init__(self, play_list_name: str, description: str):
         """creates a playlist object"""
-        self.name = play_list_name
-        self.description = description
+        self._name = play_list_name
+        self._description = description
         self._usage = UsageStats(datetime.now())
-        self.playlist = []
+        self._playlist = []
 
     @property
     def name(self):
         """getter for the playlist name"""
-        return self.name
+        return self._name
 
     @name.setter
     def name(self, new_name):
@@ -30,7 +30,7 @@ class PlayList:
     @property
     def description(self):
         """getter for the playlist description"""
-        return self.description
+        return self._description
 
     @description.setter
     def description(self, new_description):
@@ -40,67 +40,74 @@ class PlayList:
     def add_song(self, song: Song, posn: int = None):
         """add a song to the playlist"""
         if posn is not None:
-            if len(self.playlist) > posn >= 0:
-                self.playlist.insert(posn, song)
+            if len(self._playlist) > posn >= 0:
+                self._playlist.insert(posn, song)
             else:
-                raise ValueError("Position", posn, "is not available for", self.name)
+                raise ValueError("Position", posn, "is not available for", self._name)
         else:
-            self.playlist.append(song)
+            self._playlist.append(song)
 
     def remove_song(self, song: Song):
         """removes a song from the playlist"""
         try:
-            self.playlist.remove(song)
+            self._playlist.remove(song)
         except ValueError:
-            print(song, "is not in the playlist", self.name)
+            print(song, "is not in the playlist", self._name)
 
     def move_song(self, song: Song, posn: int):
         """changes the position of the song in the playlist"""
-        if len(self.playlist) > posn >= 0:
+        if len(self._playlist) > posn >= 0:
             try:
-                self.playlist.insert(posn, self.playlist.pop(self.playlist.index(song)))
+                self._playlist.insert(posn, self._playlist.pop(self._playlist.index(song)))
             except ValueError:
-                print(song, "is not in the playlist", self.name)
+                print(song, "is not in the playlist", self._name)
         else:
-            raise ValueError("Position", posn, "is not available for", self.name)
+            raise ValueError("Position", posn, "is not available for", self._name)
 
     def list_songs(self) -> List:
+        """returns a list of the songs in the playlist"""
         song_list = []
-        for i in self.playlist:
+        for i in self._playlist:
             (song_list.append(
-                "{}{}{}{}{}".format(str(self.playlist.index(i) + 1), i._title.ljust(20), i._artist.ljust(20),
+                "{}{}{}{}{}".\
+                    format(str(self._playlist.index(i) + 1), i._title.ljust(20), i._artist.ljust(20),
                                     i._album.ljust(20), i._runtime.ljust(20))))
         return song_list
 
     def get_song_by_position(self, posn: int) -> Song:
-        if len(self.playlist) > posn >= 0:
-            return self.playlist[posn]
-        else:
-            raise ValueError("Position", posn, "is not available for", self.name)
+        """returns a song object corresponding to the posn specified"""
 
-    def find_song(self, title: str = None, artist: str = None, album: str = None) -> int:
+        if len(self._playlist) > posn >= 0:
+            return self._playlist[posn]
+        else:
+            raise ValueError("Position", posn, "is not available for", self._name)
+
+    def find_song(self, title: str = None, artist: str = None, album: str = None) -> int or None:
         """Returns a song that matches the title, artist and/or album"""
 
         if title is not None:
-            by_title = set([self.playlist.index(song) for song in self.playlist if song._title == title])
+            by_title = set([self._playlist.index(song) for song in self._playlist if song._title == title])
         else:
-            by_title = set([i for i in range(len(self.playlist))])
+            by_title = set([i for i in range(len(self._playlist))])
         if album is not None:
-            by_album = set([self.playlist.index(song) for song in self.playlist if song._album == album])
+            by_album = set([self._playlist.index(song) for song in self._playlist if song._album == album])
         else:
-            by_album = set([i for i in range(len(self.playlist))])
+            by_album = set([i for i in range(len(self._playlist))])
         if artist is not None:
-            by_artist = set([self.playlist.index(song) for song in self.playlist if song._artist == artist])
+            by_artist = set([self._playlist.index(song) for song in self._playlist if song._artist == artist])
         else:
-            by_artist = set([i for i in range(len(self.playlist))])
+            by_artist = set([i for i in range(len(self._playlist))])
         possible_songs = list(by_title.intersection(by_album, by_artist))
 
-        return possible_songs[0]
+        if len(possible_songs) > 0:
+            return possible_songs[0]
+        else:
+            return None
 
     def number_of_songs(self) -> int:
         """returns the number of songs stored in the playlist"""
 
-        return len(self.playlist)
+        return len(self._playlist)
 
     def update_usage_stats(self):
         """updates the play count and the last played data in the UsageStats object"""
