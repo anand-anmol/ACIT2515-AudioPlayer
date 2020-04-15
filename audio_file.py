034fa6fc7b428fb991f6f758b130c8daaaa06519
Anmol Anand <anand.anmol150@gmail.com>
2020-04-15 03:48:38 -0700

    saving progress for model

---
+++ audio_file.py
@@ -0,0 +1,86 @@
+from datetime import datetime, time
+from os import path
+from typing import Dict
+from usage_stats import UsageStats
+from abc import abstractmethod
+from base import Base
+
+
+class AudioFile(Base):
+    """Models an abstract playable audio file.
+
+    Author: Anmol Anand(A01174846), Nicholas Janus(A01179897).
+    """
+    """ ORM: map db columns to instance variables in this class """
+
+    __tablename__ = "song_tbl"
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, nullable =False)
+    artist = Column(Text, nullable=False)
+    file_location = Column(Text, nullable=False)
+    runtime = Column(Text)
+    date_added = Column(Text, nullable=False)
+    play_count = Column(Integer, nullable=False)
+    last_played = Column(Text)
+    rating = Column(Integer)
+
+    def __init__(self, title: str, artist: str, runtime: str, file_location: str):
+        """Create a new audio instance"""
+        if not isinstance(title, str):
+            raise ValueError("title of the song must be a string")
+        if not isinstance(artist, str):
+            raise ValueError("the artist must be a string")
+        if not self.set_runtime(runtime):
+            raise ValueError("the runtime must be in the format hh:mm:ss")
+        if not isinstance(file_location, str):
+            raise ValueError("the file location must be a string")
+        self._title = title
+        self._artist = artist
+        self._rating = None
+        self._file_location = file_location
+        self._usage = UsageStats(datetime.now())
+        if type(self) == AudioFile:
+            raise NameError("cannot create an AudioFile object")
+
+    @property
+    def rating(self) -> float:
+        """gets value for user ratings"""
+        return self._rating
+
+    @rating.setter
+    def rating(self, value: float):
+        """sets the value of user ratings"""
+        self._rating = value
+
+    def update_usage_stats(self):
+        """plays the audo file"""
+        self._usage.increment_usage_stats()
+
+    @abstractmethod
+    def get_description(self) -> str:
+        """returns a description of the audio file"""
+        pass
+
+    def get_location(self) -> str:
+        """ returns location of audio file """
+        return self._pathname + self._filename
+
+    def get_usage_stats(self) -> UsageStats:
+        """returns stats about audio file"""
+        return self._usage
+
+    @abstractmethod
+    def meta_data(self) -> Dict:
+       pass
+
+    def get_play_count(self):
+        return self._usage.play_count
+
+    def set_runtime(self, runtime: str) -> bool:
+        """sets runtime if valid and returns True, if not returns False"""
+        time_formats = ['%S', '%M:%S','%H:%M:%S']
+        try:
+            self._runtime = datetime.strptime(runtime, time_formats[runtime.count(":")]).time()
+            return True
+        except ValueError:
+            return False
---
+++ song.py
@@ -0,0 +1,89 @@
+from audio_file import AudioFile
+from datetime import datetime, time
+from typing import Dict
+from usage_stats import UsageStats
+
+class Song(AudioFile):
+    """ Models a single song that can be played by an audio player.
+    
+    Author: Anmol Anand(A01174846), Nicholas Janus(A01179897).
+    """
+
+    """ ORM: map db columns to instance variables in this class """
+    album = Column(Text)
+    genre = Column(Text)
+
+    def __init__(self, title: str, artist: str, album: str, runtime: str, file_location: str,  genre: str = None):
+        """Creates an instance of a song"""
+
+        if not isinstance(album, str):
+            raise ValueError("album must be a string")
+        if genre is not None and isinstance(genre, str):
+            self._genre = genre
+        else:
+            self._genre = None
+
+        super().__init__(title, artist, runtime, file_location)
+        self._album = album
+
+    @property
+    def album(self) -> str:
+        """returns the song._album or sets the song._album"""
+        return self._album
+
+    @album.setter
+    def album(self, update: str):
+        self._album = update
+
+    @property
+    def genre(self) -> str:
+        """returns the song._genre or sets the song._genre"""
+        return self._genre
+
+    @property
+    def runtime(self) -> str:
+        """returns the runtime for the song object"""
+
+        return self._runtime
+
+    @genre.setter
+    def genre(self, update: str):
+        try:
+            if self._genre is not None and update.strip() not in self._genre:
+                self._genre += ",", update
+            else:
+                self._genre = update
+        except TypeError:
+            print("genre must be string")
+
+    def meta_data(self) -> Dict:
+        """returns a dictionary of song meta data"""
+        meta_dict = {
+            "title": str(self._title),
+            "artist": str(self._artist),
+            "album": str(self._album),
+            "date_added": str(self._usage.date_added),
+            "runtime": str(self._runtime),
+            "pathname": str(self._pathname),
+            "filename": str(self._filename),
+            "genre": str(self._genre),
+            "play_count": str(self._usage.play_count),
+            "last_played": str(self._usage.last_played),
+            "rating": str(self._rating)
+        }
+        return meta_dict
+
+    def get_description(self):
+        """prints out details about the song object"""
+        try:
+            song_details = "{} by {} from the album {} added on {}. Runtime is {}." \
+                .format(self._title, self._artist, self._album, self._usage._date_added, self._runtime)
+
+            if self._usage.last_played is not None:
+                song_details += " Last played on " + str(self._usage.last_played) + "."
+            if self.rating is not None:
+                song_details += " User rating is " + str(self.rating) + "/5."
+        except AttributeError:
+            song_details = "Song is missing info, cannot display"
+
+        return song_details
